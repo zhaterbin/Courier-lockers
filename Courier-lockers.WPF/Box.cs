57e6b85a63e6ad6e20d986817dabe5dc722c0d1c
邵家驹 <987585547@qq.com>
2024-04-11 18:31:09 +0800

    wpf ui

---
+++ Courier-lockers.WPF/Box.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courier_lockers.WPF
+{
+   public partial class Box:ObservableObject
+    {
+        [ObservableProperty]
+        int id;
+        [ObservableProperty]
+        bool isOccupied;
+        [ObservableProperty]
+        bool isOpened;
+        [ObservableProperty]
+        bool isChecking;
+    }
+}
---
+++ Courier-lockers.WPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MahApps.Metro.Controls;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Courier_lockers.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void Border_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton==MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
